d8b751abb265a3aa5f0b12100897b39741281abf
VladKuvshinov <vlad.kuvshinovv.2000@gmail.com>
2023-12-09 04:51:59 +0300

    feat:lab5

--- WebLab_5/js/countdown.js
+++ WebLab_5/js/countdown.js
@@ -2,6 +2,7 @@
     var start = Date.now()
     window.addEventListener('load', function() {
         let res = new Date()
-        document.getElementById('countDown').textContent =  31 - res.getDay() + 4
+        let count = 31 - res.getUTCDay() - 3
+        document.getElementById('countDown').textContent =  count
     });
 })();
---
+++ WebLab_5/js/feedBackScript.js
@@ -0,0 +1,59 @@
+document.getElementById('tableForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const tableType = document.getElementById('tableType').value;
+    const quantity = document.getElementById('quantity').value;
+    const language = document.getElementById('language').value;
+    localStorage.setItem('tableType', tableType);
+    localStorage.setItem('quantity', quantity);
+    localStorage.setItem('language', language);
+    const tableContainer = document.getElementById('tableContainer');
+    tableContainer.innerHTML = generateTable(tableType, quantity, language);
+
+
+
+});
+
+
+function generateTable() {
+    let tableType = localStorage.getItem("tableType")
+    let quantity = localStorage.getItem("quantity")
+    let language = localStorage.getItem('language')
+    let scheduleTable = '<table>';
+    for (let i = 0; i < quantity; i++) {
+        scheduleTable += '<tr>';
+            if(tableType === "goodOnes") {
+                if (language === "english") {
+                    scheduleTable += '<td>user223917249821: Good!</td>';
+
+                } else {
+                    scheduleTable += '<td>user223917249821: Отлично!</td>';
+                }
+
+
+            }else {
+                if (language === "english") {
+                    scheduleTable += '<td> hater232: Bad!</td>';
+
+                } else {
+                    scheduleTable += '<td> hater2232: Ужасно!</td>';
+                }
+
+
+            }
+
+        scheduleTable += '</tr>';
+    }
+    scheduleTable += '</table>';
+
+  return scheduleTable
+
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('tableType').value = localStorage.getItem('tableType') || 'goodOnes';
+    document.getElementById('maxLessons').value = localStorage.getItem('maxLessons') || '';
+    document.getElementById('language').value = localStorage.getItem('language') || '';
+});
